d38627d78a26ee56e64f39d90baaff2fea0fcb80
rnoguer22 <rnogugon@myuax.com>
2022-05-07 18:54:24 +0200

    Grafico de dispersion

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from Clases.Grafico import Grafico
 
-grafica = Grafico("Dataset housing/USA_Housing.csv")
-grafica.grafico("bar","Area Population","Price")
+if __name__ == "__main__":
+    #Definimos grafica como instancia de la clase Grafico
+    grafica = Grafico("Dataset housing/USA_Housing.csv")
+    #Grafico para ver la relacion entre lo habitada que esta un area y su precio
+    #grafica.grafico("bar","Area Population","Price")
+    #Grafico de dispersion para comparar el precio del area y el numero de dormitorios
+    grafica.grafico("dispersion","Avg. Area Income","Avg. Area Number of Rooms")
