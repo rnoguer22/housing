52945fc3b0555d7bdc490162a51f4c1fcbd72574
rnoguer22 <rnogugon@myuax.com>
2022-05-07 12:11:08 +0200

    Metodo grafico para crear graficos con matplotlib

--- Analisis_EDA.py
+++ Analisis_EDA.py
@@ -7,4 +7,14 @@ class Grafico:
         self.archivo = archivo
         self.col1 = col1
         self.col2 = col2
+        self.dt = pd.read_csv(archivo)
     
+    def grafico(self, tipo_grafico):
+        fig, ax = plt.subplots()
+        if tipo_grafico == "dispersion":
+            ax.scatter(self.dt[self.col1], self.dt[self.col2])
+        else:
+            self.dt.plot(kind = tipo_grafico, ax = ax)
+        ax.set_title('Grafico ' + tipo_grafico, loc = "center", fontdict = {'fontsize':14, 'fontweight':'bold', 'color':'tab:blue'})
+        ax.set_ylabel('')
+        plt.savefig('Graficos/grafico' + ' - '.join(tipo_grafico) + '.png', bbox_inches='tight')
