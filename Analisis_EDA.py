b6366ac3c73e7fc8d77d5e0555f1252d61efcad1
rnoguer22 <rnogugon@myuax.com>
2022-05-07 12:19:48 +0200

    Generamos el primer grafico A su vez mejorramos el metodo grafico para un mejor funcionamiento

--- Analisis_EDA.py
+++ Analisis_EDA.py
@@ -3,18 +3,20 @@ import matplotlib.pyplot as plt
 
 class Grafico:
     #Constructor
-    def __init__(self, archivo, col1, col2):
+    def __init__(self, archivo):
         self.archivo = archivo
-        self.col1 = col1
-        self.col2 = col2
         self.dt = pd.read_csv(archivo)
     
-    def grafico(self, tipo_grafico):
+    def grafico(self, tipo_grafico, col1, col2):
+        print("Generando Grafico...")
         fig, ax = plt.subplots()
         if tipo_grafico == "dispersion":
-            ax.scatter(self.dt[self.col1], self.dt[self.col2])
+            ax.scatter(self.dt[col1], self.dt[col2])
         else:
-            self.dt.plot(kind = tipo_grafico, ax = ax)
+            self.dt.groupby(col1)[col2].plot(kind = tipo_grafico, ax = ax)
         ax.set_title('Grafico ' + tipo_grafico, loc = "center", fontdict = {'fontsize':14, 'fontweight':'bold', 'color':'tab:blue'})
         ax.set_ylabel('')
         plt.savefig('Graficos/grafico' + ' - '.join(tipo_grafico) + '.png', bbox_inches='tight')
+
+grafica = Grafico("Dataset housing/USA_Housing.csv")
+grafica.grafico("bar","Area Population","Price")
