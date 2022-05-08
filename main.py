6d6dd189c5f10478af262af1e7681d72ca1c0f28
rnoguer22 <rnogugon@myuax.com>
2022-05-08 10:34:02 +0200

    Cambios en los graficos

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Clases.Grafico import Grafico
 
 if __name__ == "__main__":
+
     #Definimos grafica como instancia de la clase Grafico
     grafica = Grafico("Dataset housing/USA_Housing.csv")
     #Grafico para ver la relacion entre lo habitada que esta un area y su precio
@@ -9,7 +10,9 @@ if __name__ == "__main__":
     grafica.grafico("dispersion","Avg. Area Income","Avg. Area Number of Rooms")
     #A continuacion vamos a comparar el ingreso medio con el precio de la vivienda adquirida
     grafica.grafico("dispersion","Avg. Area Income","Price")
-    #Aanalizamos el numero de dormitorios con el numero de habitaciones que tiene la vivienda
-    grafica.grafico("dispersion","Avg. Area Number of Rooms","Avg. Area Number of Bedrooms")
+    #Aanalizamos el numero de dormitorios con el precio de la vivienda
+    grafica.grafico("dispersion","Avg. Area Number of Rooms","Price")
 
     #Podemos observar que la correlacion corresponde a simple vista con cada grafico
+    #Cabe destacar el uso unicamente de graficos de dispersion para ver a simple vista si
+    #el valor que va a tomar la correlacion aproximadamente
